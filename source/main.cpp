647eaf6a62291e1ec52857fe1bb56220fb2cc371
Martin Wright <killerninjacodemonkey@gmail.com>
2014-10-15 00:17:33 +0100

    More Project Settings
    Project adjustments and assert testing.

--- source/main.cpp
+++ source/main.cpp
@@ -4,9 +4,13 @@
 //------------------------------------------------------------------------------
 #include <Windows.h>
 
+#include "kernel\assert.h"
+
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
 {
+	peasoup::Assert::getInstance().reportError("test 1", 1, "test 2", "test %d", 3);
+
 	return 0;
 }
